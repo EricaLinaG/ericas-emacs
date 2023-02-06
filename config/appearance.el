e8c250b7697a9fe8426f4f0e317c28812b209109
ericalinag <ericalinagebhart@gmail.com>
2023-02-06 03:50:43 +0000

    change font size

--- config/appearance.el
+++ config/appearance.el
@@ -49,7 +49,7 @@
 
 ;;(set-frame-font "-*-Source Code Pro-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1")
 ;;(set-frame-font "-*-Iosevka-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1")
-(set-frame-font "Iosevka term 12")  ; 12 is size, not height. height is more.
+(set-frame-font "Iosevka term 10")  ; 12 is size, not height. height is more.
 
 ;;make fringe bigger
 (if (fboundp 'fringe-mode)
