7e79a42630b8931a0e933fd0eb02166dbce3437f
ericalinag <ericalinagebhart@gmail.com>
2023-02-11 19:51:05 +0000

    tweaks to font and dired

--- config/dired-conf.el
+++ config/dired-conf.el
@@ -4,7 +4,13 @@
 (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
 (dired-async-mode 1)
 
+(setq dired-create-destination-dirs 'always)
 ;; I have git gutter already.
 ;; (global-diff-hl-mode)
 ;; (add-hook 'prog-mode-hook 'diff-hl-mode)
 ;; (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+
+(add-to-list 'auto-mode-alist '("\\.\\(cbr\\)\\'" . archive-mode))
+
+(provide 'dired-conf)
+;;; dired-conf.el ends here
